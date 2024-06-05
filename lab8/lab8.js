149428f49ee3b06843209f11741ff73face265ec
sickhexd <sabiaflex0@gmail.com>
2024-06-05 19:37:45 +0700

    Исправление ошибок

--- lab8/lab8.js
+++ lab8/lab8.js
@@ -3,12 +3,12 @@ function showDate() {
     let today = new Date();
     let output = '';
 
-    output = 'Дата и время для русской локали: ' + today.toLocaleString('ru-RU') + '<br>';
-    output += 'Дата и время для армянской локали: ' + today.toLocaleString('hy-AM') + '<br>';
-    output += 'Дата и время для ливанской локали: ' + today.toLocaleString('ar-LB') + '<br>';
-    output += 'Дата и время для французской локали: ' + today.toLocaleString('fr-FR') + '<br>';
-    output += 'Дата и время для сербской локали: ' + today.toLocaleString('sr-SP') + '<br>';
-    output += 'Дата и время для греческой локали: ' + today.toLocaleString('el-GR');
+    output = 'Дата и время для России: ' + today.toLocaleString('ru-RU') + '<br>';
+    output += 'Дата и время для Америки: ' + today.toLocaleString('en-US') + '<br>';
+    output += 'Дата и время для Германии: ' + today.toLocaleString('de-DE') + '<br>';
+    output += 'Дата и время для Франции: ' + today.toLocaleString('fr-FR') + '<br>';
+    output += 'Дата и время для Сербии: ' + today.toLocaleString('sr-SP') + '<br>';
+    output += 'Дата и время для Японии: ' + today.toLocaleString('ja-JA');
     out.innerHTML = output;
 }
 
